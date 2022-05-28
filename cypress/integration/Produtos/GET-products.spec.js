93b753ce80e4662bb46a642f5dd93d9b69680a54
Murillo Welsi <murillo.welsi@gmail.com>
2022-05-28 19:29:23 +0100

    added products tests

---
+++ cypress/integration/Produtos/GET-products.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe("Given the products api", () => {
+  context("When I send GET /produtos", () => {
+    it("Then it should return a list with all registered products", () => {
+      cy.request({
+        method: "GET",
+        url: "/produtos",
+      }).should((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.quantidade).to.eq(response.body.produtos.length);
+
+        Cypress._.each(response.body.produtos, (produto) => {
+          expect(produto.email).to.not.be.null;
+          expect(produto).to.have.all.keys(
+            "nome",
+            "preco",
+            "descricao",
+            "quantidade",
+            "_id"
+          );
+        });
+      });
+    });
+  });
+});
