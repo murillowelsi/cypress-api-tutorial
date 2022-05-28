6d372ecd5016de15ea06f9006d465dd8778b2f6f
Murillo Welsi <murillo.welsi@gmail.com>
2022-05-28 19:35:41 +0100

    added carts tests

---
+++ cypress/integration/Carrinhos/GET-carts.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("Given the carts api", () => {
+  context("When I send GET /carrinhos", () => {
+    it("Then it should return a list with all registered products", () => {
+      cy.request({
+        method: "GET",
+        url: "/carrinhos",
+      }).should((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.quantidade).to.eq(response.body.produtos.length);
+      });
+    });
+  });
+});
